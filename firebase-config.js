48bb73ca7c0043d6fcb853ae801fe158b7f90cc9
Jrvl83 <jrvargasleon@gmail.com>
2025-10-15 23:49:41 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Configuración de Firebase para ChambApp
+const firebaseConfig = {
+  apiKey: "AIzaSyD5sAUSVp53lO0fVoHmFdlZwNOuctjREeM",
+  authDomain: "chambapp-7785b.firebaseapp.com",
+  projectId: "chambapp-7785b",
+  storageBucket: "chambapp-7785b.firebasestorage.app",
+  messagingSenderId: "986133070577",
+  appId: "1:986133070577:web:91d07708f067ea91ac5f50"
+};
+
+// Exportar configuración
+window.firebaseConfig = firebaseConfig;
